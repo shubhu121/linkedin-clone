5d586196d42d30d967d65f6809d01957288b29d4
shubhu121 <shubhucsrldelhi@gmail.com>
2023-08-26 02:03:14 +0530

    adding passport configuration

--- linkedin-clone-backend/index.js
+++ linkedin-clone-backend/index.js
@@ -1,14 +1,40 @@
 const express = require("express");
+const passport = require("passport");
+const { ExtractJwt } = require("passport-jwt");
 const app = express();
 
+// passport jwt setup
+
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secret0rKey = "topsecret";
+passport.use(
+    new JwtStrategy(opts, function (jwt_payload, done) {
+        User.findOne({_id: jwt_payload.identifier}, function(err, user) {
+            if(err) {
+                done(err, false);
+            }
+            if(user) {
+                done(null, user);
+            }
+            else {
+                done(null, false);
+            }
+
+        });
+    })
+);
+
 app.get("/", (req, res) => {
-    res.send("I am working");
+  res.send("I am working");
 });
 
-app.get*("/hello", (req, res) => {
+app.get *
+  ("/hello",
+  (req, res) => {
     res.send("Hello world! This is the new route!");
-});
+  });
 
 app.listen(8000, () => {
-    console.log("Server running on port 8000");
+  console.log("Server running on port 8000");
 });
